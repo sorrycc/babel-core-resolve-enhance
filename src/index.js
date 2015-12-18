6ad46048256d579dd9d209c1a23c13ed452bd63d
sorrycc <sorrycc@gmail.com>
2015-12-18 10:29:03 +0800

    fix: don't work in windows, Fix #1

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 import * as resolve from './resolve';
 import { join } from 'path';
 
+function winPath(path) {
+  return path.replace(/\\/g, '/');
+}
+
 export default function hackResolve(opts) {
   if (!opts || !opts.dirname) {
     throw new Error('[Error] opts.dirname must be supplied');
@@ -10,8 +14,8 @@ export default function hackResolve(opts) {
 
   var realRequire = require.extensions[".js"];
   require.extensions[".js"] = function(m, filename) {
-    if (filename.indexOf('babel-core/lib/helpers/resolve.js') > -1) {
-      if (m.parent && m.parent.id.indexOf('babel-core/lib/transformation/file/options/option-manager.js') > -1) {
+    if (winPath(filename).indexOf('babel-core/lib/helpers/resolve.js') > -1) {
+      if (m.parent && winPath(m.parent.id).indexOf('babel-core/lib/transformation/file/options/option-manager.js') > -1) {
         var content = require('fs').readFileSync(join(__dirname, 'resolve.js'), 'utf-8');
         return m._compile(content, filename);
       }
